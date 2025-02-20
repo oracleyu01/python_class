8eacba6e43eb886bb657bfa8f9000980504e904e
oracleyu01 <oracleyu23@gmail.com>
2025-02-21 08:58:17 +0900

    Update and rename 예제9.py to 예제10.철자를_세는_함수_len_사용하기.py

---
+++ 1유형/예제10.철자를_세는_함수_len_사용하기.py
@@ -0,0 +1,29 @@
+
+▩ 예제10.철자를_세는_함수_len_사용하기 
+
+문법:  이름과 이름의 철자의 갯수를 출력하시오 !
+
+SQL> select  ename , length(ename)
+            from  emp;
+
+답:
+
+
+
+문제1. 아래의 SQL을 판다스로 구현하시오 !
+
+SQL> select  ename, job, length(job)
+           from  emp
+           order  by  length(job)  desc; 
+
+답:
+
+
+
+문제2.  train.csv 에서 중개사무소, 중개사무소 철자의 길이를 출력하는데
+          중개 사무소 철자의 길이가 긴것부터 출력하시오 !
+
+
+답:  
+
+
