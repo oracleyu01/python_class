6f32b975d7e66b542cd8cac52ca1f1edc9074693
oracleyu01 <oracleyu23@gmail.com>
2025-02-21 08:56:31 +0900

    Update and rename 예제10.py to 예제09.문자열_잘라내기.py

---
+++ 1유형/예제09.문자열_잘라내기.py
@@ -0,0 +1,17 @@
+
+▩ 예제09.문자열_잘라내기.py 
+
+문법:  이름의 첫번째 철자만 잘라내서 출력하시오 !
+
+SQL>  select  substr( ename, 1, 1 )
+           from  emp;
+
+Pandas> 
+
+Pandas>  
+
+설명:  start=0 은 0번째 자리부터 시작해서 stop=1 은 1번째 자리 미만까지 
+
+문제1.  train.csv 에서  방향이 남동으로 시작하는 행의 모든 열을 전부 출력하시오! 
+
+Pandas>
