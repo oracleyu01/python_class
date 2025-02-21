7a791c3bbf396abf94315bee1386f83fae19ac47
oracleyu01 <oracleyu23@gmail.com>
2025-02-21 16:17:08 +0900

    Create 예제18.최대값 출력하기_(max).py

---
+++ 1유형/예제18.최대값 출력하기_(max).py
@@ -0,0 +1,30 @@
+
+▩ 예제18.최대값 출력하기_(max).py
+
+   오라클                  vs               판다스 
+
+     max                                    emp['sal'].max()
+     min                                    emp['sal'].min()
+     sum                                   emp['sal'].sum()
+     avg                                    emp['sal'].mean()
+    count                                  emp['sal'].count()
+
+예제. 아래의 SQL을 판다스로 구현하시오 
+
+SQL> select  max(sal)
+           from  emp;
+
+답:   
+
+문제1.  아래의 SQL 을 판다스로 구현하시오 !
+
+SQL>  select  max(sal)
+          from  emp
+          where  job='SALESMAN';
+
+답:  
+
+
+
+
+
