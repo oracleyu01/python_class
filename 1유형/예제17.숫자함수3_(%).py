b28fb70183af9bb86ceffd30b04f8d5415e2d8b0
oracleyu01 <oracleyu23@gmail.com>
2025-02-21 16:10:56 +0900

    Create 예제17.숫자함수3_(%).py

---
+++ 1유형/예제17.숫자함수3_(%).py
@@ -0,0 +1,24 @@
+
+▩ 예제17.숫자함수3_(%).py
+
+   오라클               vs           파이썬 
+    mod                                 % 
+
+예제.  아래의 SQL을 파이썬으로 구현하시오 !
+
+select  mod(10,3)  from  dual;
+
+답:    
+
+문제1. 아래의 SQL을 판다스로 구현하시오 !
+
+SQL>   SELECT  empno, ename, sal
+             from  emp
+            where  mod(empno, 2 ) = 0;
+
+답:
+
+
+
+
+
