4a618fd1600541a03acb04d311daf4aaeae52586
oracleyu01 <oracleyu23@gmail.com>
2025-02-23 09:42:41 +0900

    Create 예제44.빅데이터_분석_기사_실기_시험_작업형_1번_문제_풀기.py

---
+++ 1유형/예제44.빅데이터_분석_기사_실기_시험_작업형_1번_문제_풀기.py
@@ -0,0 +1,17 @@
+
+▩ 예제44.빅데이터_분석_기사_실기_시험_작업형_1번_문제_풀기.py
+
+시험환경 체험하기 : https://cafe.daum.net/oracleoracle/Sq8G/32
+
+예제. 제공딘 데이터 mtcars.csv 의 qsec 컬럼을 최소-최대 척도(min max scale) 로
+        변환한수 0.5 보다 큰 값을 가지는 레코드 수를 출력하시오 !
+
+#1. 데이터 불러오기 
+import pandas  as  pd
+mtcars = pd.read_csv("d:\\data\\mtcars.csv")
+#mtcars.head()
+
+
+
+문제1. 빅분기 시험 환경 작업형 1번 문제를 풀고 숫자 9가 출력된 화면을 캡쳐해서 제출하시오 !
+
