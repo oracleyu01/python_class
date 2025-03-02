c184fc733b0cbbf34f8d9cdadd0fc4d33cce82a8
oracleyu01 <oracleyu23@gmail.com>
2025-03-02 17:38:57 +0900

    Update 예제76.py

--- 파이썬/예제76.py
+++ 파이썬/예제76.py
@@ -7,36 +7,15 @@
 
 예제1. 리스트에서 짝수의 갯수를 세기 
 
-num = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-count = 0 
 
-for  i   in   num:
-    if  i % 2 == 0:
-        count =  count + 1 
 
-print ('짝수의 갯수는' , count )
 
 문제1. 동전을 10번 던져서 앞면 또는 뒷면을 출력하시오 !
 
-import  random
 
-coin = ['앞면', '뒷면']
 
-for  i   in  range(1, 11):
-    result = random.choice(coin)
-    print( result )
     
 문제2.  동전을 10번 던져서 앞면이 나오는 횟수를 출력하시오 !
 
-import  random
 
-coin = ['앞면', '뒷면']
-count = 0   
-
-for  i   in  range(1, 11):
-    result = random.choice(coin)
-    if  result =='앞면':
-        count = count + 1
-
-print( count )
 
